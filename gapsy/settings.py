1e451b29f3efaea08302ac58b759f9f6d2533ffa
mariano <mariano_fferrer@hotmail.com>
2023-10-04 17:33:22 -0300

    first commit

--- gapsy/settings.py
+++ gapsy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wsx^k#1$qla&cr-ucwo$tws&bj75daceuag6b2f(@$7vw*zm%8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
