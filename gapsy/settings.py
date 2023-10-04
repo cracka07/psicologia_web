505b15592c0f07ceaf61353a0184ed078524fd30
mariano <mariano_fferrer@hotmail.com>
2023-10-04 13:14:55 -0300

    first commit

--- gapsy/settings.py
+++ gapsy/settings.py
@@ -125,13 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS =[
-    'gapsy/static'
-]
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# STATIC_URL = "static/"
+# STATIC_ROOT = BASE_DIR / "static"
+# STATICFILES_DIRS =[
+#     'gapsy/static'
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # GOOGLE_DRIVE_CREDENTIALS_PATH = os.path.join(BASE_DIR, 'credentials', 'client_secrets.json')
 
