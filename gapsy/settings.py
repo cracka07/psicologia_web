492b54edf9c79deda3634e6aaf74784c84b8ed72
mariano <mariano_fferrer@hotmail.com>
2023-10-05 01:28:29 -0300

    renew commit

--- gapsy/settings.py
+++ gapsy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wsx^k#1$qla&cr-ucwo$tws&bj75daceuag6b2f(@$7vw*zm%8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,9 +127,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS =[
-     'gapsy/static'
- ]
+# STATICFILES_DIRS =[
+#      'gapsy/static'
+#  ]
+
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
