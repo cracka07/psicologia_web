ff07a2a40e04aebede19af09d20d6ac6ab6048bd
mariano <mariano_fferrer@hotmail.com>
2023-10-05 18:38:22 -0300

    new deploy

--- gapsy/settings.py
+++ gapsy/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,15 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 # STATICFILES_DIRS =[
 #      'gapsy/static'
 #  ]
-STATICFILES_DIRS = [
-    BASE_DIR / 'gapsy/static',
-]
+#STATICFILES_DIRS = [
+   # BASE_DIR / 'gapsy/static',
+#]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
