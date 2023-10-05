2658b9bc3946e0b64ad336a5d1bc4c4fed31c75b
mariano <mariano_fferrer@hotmail.com>
2023-10-05 18:04:45 -0300

    new deploy

--- gapsy/settings.py
+++ gapsy/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wsx^k#1$qla&cr-ucwo$tws&bj75daceuag6b2f(@$7vw*zm%8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
 DEBUG = False
+#DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -126,11 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS =[
-     'gapsy/static'
- ]
+# STATICFILES_DIRS =[
+#      'gapsy/static'
+#  ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'gapsy/static',
+]
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
