f75edfcfa0ded30934f8ced1d835bb52cd79a345
mariano <mariano_fferrer@hotmail.com>
2023-12-03 21:44:43 -0300

    saved

--- miscelanea/views.py
+++ miscelanea/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect
 
 def administracion_miscelanea(request):
+    # Definir el ID de la carpeta de Google Drive que se va a enlazar
     folder_id = "1JuZdzpu84Uh1pIsxvUqa8Q4hyGahr1gt"
-
     # Construir la URL de la carpeta de Google Drive
     drive_folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
-
+    # Redirigir al usuario a la URL de la carpeta de Google Drive
     return redirect(drive_folder_url)
--- recursos/views.py
+++ recursos/views.py
@@ -1,36 +1,14 @@
 
-from django.shortcuts import redirect, render
-import requests
+# Importar la función 'redirect' desde el módulo 'shortcuts' de Django
+from django.shortcuts import redirect,render
 
-
-def obtener_subcarpetas(folder_id, access_token):
-    url = f"https://www.googleapis.com/drive/v3/files?q='{folder_id}'+in+parents"
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    response = requests.get(url, headers=headers)
-    contenido = response.json()
-    subcarpetas = contenido.get("files", [])
-   
-    return subcarpetas
-def obtener_contenido_carpeta(carpeta_id, access_token):
-    url = f"https://www.googleapis.com/drive/v3/files?q='{carpeta_id}'+in+parents"
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    response = requests.get(url, headers=headers)
-    contenido = response.json()
-    subcarpetas = contenido.get("files", [])
-   
-    return subcarpetas
 
 def administracion(request):
+    # Definir el ID de la carpeta de Google Drive que se va a enlazar
     folder_id = "1bFX5WvZAARs1wyj8wl8L-PAIn3QTlV_c"
-
     # Construir la URL de la carpeta de Google Drive
     drive_folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
-
-   
+    # Redirigir al usuario a la URL de la carpeta de Google Drive
     return redirect(drive_folder_url)
 
 def admin(request):
